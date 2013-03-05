f8d7714abbff779999fc4c8426d82d7c2627453b
Matthew de Verteuil <matthew@jazinga.com>
2013-03-05 13:39:11 -0500

    Added configurable request timeout to transport mechanisms with a default value of 10 seconds.

--- refreshbooks/transports/use_urllib2.py
+++ refreshbooks/transports/use_urllib2.py
@@ -2,16 +2,21 @@ import urllib2 as u
 
 from refreshbooks import exceptions as exc
 
+
 class Transport(object):
-    def __init__(self, url, headers_factory):
+    def __init__(self, url, headers_factory, timeout=10):
         self.url = url
         self.headers_factory = headers_factory
-    
+        self.timeout = timeout
+
     def __call__(self, entity):
-        request = u.Request(
-            url=self.url,
-            data=entity,
-            headers=self.headers_factory()
+        request = u.openurl(
+            u.Request(
+                url=self.url,
+                data=entity,
+                headers=self.headers_factory()
+            ),
+            timeout=self.timeout
         )
         try:
             return u.urlopen(request).read()
